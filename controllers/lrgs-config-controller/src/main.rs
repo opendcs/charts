cff68eeb901d8831c0c978a4645f746b12455fee
Mike Neilson <m.allan.neilson@gmail.com>
2025-02-17 04:17:25 -0800

    LRGS password file is written.

--- controllers/lrgs-config-controller/src/main.rs
+++ controllers/lrgs-config-controller/src/main.rs
@@ -1,13 +1,15 @@
 use api::v1::dds_recv::DdsConnection;
 use k8s_openapi::api::core::v1::Secret;
 //use futures::{StreamExt, TryStreamExt};
-use kube::{Client, api::{Api, ListParams }};//, PostParams}};
+use kube::{Client, api::{Api, ListParams }};
+//, PostParams}};
 use std::fs::File;
 use std::error::Error;
 use simple_xml_builder::XMLElement;
-use sha1::{Sha1, Digest};
+
 
 mod api;
+mod password_file;
 
 use clap::Parser;
 
@@ -25,15 +27,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let client = Client::try_default().await?;
 
     let file = File::create(args.conf_dir.join("ddsrecv.conf"))?;
-    create_ddsrecv_conf(&client, &file).await?;
+    create_ddsrecv_conf(&client, file).await?;
 
     let pw_file = File::create(args.conf_dir.join(".lrgs.passwd"))?;
-    create_password_file(&client, &pw_file).await?;
+    create_password_file(&client, pw_file).await?;
 
     Ok(())
 }
 
-async fn create_ddsrecv_conf(client: &Client, file: &File) -> Result<(), Box<dyn Error>> {
+async fn create_ddsrecv_conf(client: &Client, file: File) -> Result<(), Box<dyn Error>> {
 
     let mut ddsrecv_conf = XMLElement::new("ddsrecvconf");
     let mut i: i32 = 0;
@@ -75,27 +77,33 @@ async fn create_ddsrecv_conf(client: &Client, file: &File) -> Result<(), Box<dyn
 }
 
 
-fn lrgs_password_hash(username: &str, password: &str) -> String {
-    let mut hasher = Sha1::new();
-    hasher.update(username);
-    hasher.update(password);
-    hasher.update(username);
-    hasher.update(password);
-    let hash = hasher.finalize();
-    base16ct::upper::encode_string(&hash)
-}
 
-async fn create_password_file(client: &Client, _file: &File) -> Result<(), Box<dyn Error>> {
+
+async fn create_password_file(client: &Client, file: File) -> Result<(), Box<dyn Error>> {
     let users: Api<Secret> = Api::default_namespaced(client.clone());
     let params = ListParams::default().fields("type=lrgs.opendcs.org/ddsuser");
+    let mut pw_file = password_file::PasswordFile::new(file);
     for user in users.list(&params).await? {
         let data = user.data;
         if data.is_some() {
             let data = data.unwrap();
             let username = String::from_utf8(data.get("username").unwrap().0.clone())?;
             let password = String::from_utf8(data.get("password").unwrap().0.clone())?;
-            println!("{} -> {}", &username, lrgs_password_hash(&username, &password));
+            let roles = data.get("roles");
+            let roles = match roles {
+                Some(_) => String::from_utf8(roles.unwrap().0.clone())?
+                               .split(",")
+                               .map(|i| String::from(i))
+                               .collect(), 
+                None => vec![]
+            };
+            pw_file.add_user(password_file::DdsUser {
+                username,
+                password,
+                roles
+            });
         }
     }
-    Ok(())
+    println!("Password file {pw_file}");
+    Ok(pw_file.write_file()?)
 }
--- controllers/lrgs-config-controller/src/password_file.rs
+++ controllers/lrgs-config-controller/src/password_file.rs
@@ -1,5 +1,65 @@
+use std::{fs::File, io::Write};
+use sha1::{Sha1, Digest};
 
+pub struct DdsUser {
+    pub username: String,
+    pub password: String,
+    pub roles: Vec<String>
+}
+
+impl std::fmt::Display for DdsUser {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"({},{},{:?})", self.username, "*********", &self.roles)
+    }
+}
 
 pub struct PasswordFile {
-    file: &File
+    file: File,
+    users: Vec<DdsUser>
+}
+
+impl PasswordFile {
+    pub fn new(file: File) -> PasswordFile {
+        PasswordFile {
+            file,
+            users: vec![]
+        }
+    }
+
+    pub fn add_user(&mut self, user: DdsUser) {
+        self.users.push(user);
+    }
+
+    pub fn write_file(&self) -> std::io::Result<()> {
+        let mut f = &self.file;
+        for user in self.users.as_slice() {
+            let pw_hash = lrgs_password_hash(&user.username, &user.password);
+            let roles = user.roles.join(",");
+            f.write_fmt(format_args!("{}:{roles}:{pw_hash}:", &user.username))?;
+        }
+        Ok(())
+    }
+}
+
+impl std::fmt::Display for PasswordFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let file = &self.file;
+        write!(f, "PasswordFile(file={file:?}, users = [")?;
+        for user in self.users.as_slice() {
+            write!(f, "{user},")?;
+        }
+        write!(f, "])")
+    }    
+}
+
+// to anyone thinking this isn't anywhere near sufficient,
+// you are correct. This is temporary to adapt a legacy system.
+fn lrgs_password_hash(username: &str, password: &str) -> String {
+    let mut hasher = Sha1::new();
+    hasher.update(username);
+    hasher.update(password);
+    hasher.update(username);
+    hasher.update(password);
+    let hash = hasher.finalize();
+    base16ct::upper::encode_string(&hash)
 }
