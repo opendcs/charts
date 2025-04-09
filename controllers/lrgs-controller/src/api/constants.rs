3d162229bb0ae79ad94a5bbafa601d770f3fbf0a
Mike Neilson <m.allan.neilson@gmail.com>
2025-04-08 20:14:10 -0700

    remove controller code.

--- controllers/lrgs-controller/src/api/constants.rs
+++
@@ -1,6 +0,0 @@
-use lazy_static::lazy_static;
-
-lazy_static! {
-    pub static ref LRGS_GROUP: String = "lrgs.opendcs.org".to_string();
-
-}
--- controllers/lrgs-controller/src/lrgs/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-pub mod password_file;
-pub mod statefulset;
-pub mod configmap;
-pub mod service;
-pub mod controller;
-pub mod state;
-pub mod telemetry;
-pub mod metrics;
